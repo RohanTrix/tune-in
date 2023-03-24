a4eb75c24df1ba3ea3f679806800e122f318f20c
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2023-03-24 22:28:34 +0530

    Implemented lazy loading of routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import SearchView from "../views/SearchView.vue";
-import NotFoundView from "../views/NotFoundView.vue";
+// import SearchView from "../views/SearchView.vue";
+// import NotFoundView from "../views/NotFoundView.vue";
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,13 +13,17 @@ export const router = createRouter({
     {
       path: "/search",
       name: "search",
-      component: SearchView,
+      component: () => import("../views/SearchView.vue"),
     },
     {
       path: "/library",
       name: "library",
-      component: NotFoundView,
+      component: () => import("../views/NotFoundView.vue"),
     },
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("../views/NotFoundView.vue"),
+    },
   ],
 });
